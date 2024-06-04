2ce26c773fa8fc26645c432a03b43a45eff16276
Idir <idir.belaid.83@gmail.com>
2024-06-04 23:21:33 +0100

    app_graphql_function

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "revisionbackend": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
